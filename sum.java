d07ea5ab35ad9bf2b45803ab41d6bdb6c7ca8a9c
AyushXX01 <r2d2ayush@gmail.com>
2023-07-29 00:51:58 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    
+    public static void main(String[] args) {
+        
+        int i = 10;
+        int j = 10;
+        int sum = i + j;
+
+        System.out.println(sum);
+    }
+}
